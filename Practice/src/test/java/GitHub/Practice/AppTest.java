5600e8c58fc7ce842bb0566081daeaaa5dbc82f7
talvamut <muthanna.talvar@diconium.com>
2025-02-16 21:19:44 +0100

    uploadProject

---
+++ Practice/src/test/java/GitHub/Practice/AppTest.java
@@ -0,0 +1,19 @@
+package GitHub.Practice;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
